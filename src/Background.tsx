c62743a17d9bfc25825fe42ddf84e5418878ad16
TauraJ <taurajgreig@outlook.com>
2024-05-13 23:38:45 +1200

    added a background

---
+++ src/Background.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Background = () => {
+  return (
+    <div className="absolute inset-0 z-[-1]">
+      <div className="w-full h-full bg-gradient-to-br from-pink-500 via-purple-500 to-indigo-500"></div>
+    </div>
+  );
+}
+
+export default Background;
